5738a116ed8ec13f510f2f8d2c3ef7f07e5a9431
Marek Gergel <mage26gg@gmail.com>
2024-02-27 18:42:56 +0100

    proj1 - elf file symbol reading
    updated Makefile for 32bit testing

--- proj1/ian_proj1.c
+++ proj1/ian_proj1.c
@@ -1,73 +1,116 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <err.h>
 #include <elf.h>
 #include <gelf.h>
 #include <libelf.h>
 
-
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s FILE\n", argv[0]);
-        exit(1);
-    }
-
-    // Open the ELF file
-    int fd = open(argv[1], O_RDONLY, 0);\
-    if (fd < 0) {
-        perror("open");
-        exit(1);
-    }
-
-    // Initialize the ELF library
-    if (elf_version(EV_CURRENT) == EV_NONE) {
-        fprintf(stderr, "ELF library initialization failed\n");
-        exit(1);
-    }
-
-    // Open the ELF file
-    Elf *elf = elf_begin((int) fd, ELF_C_READ, NULL);
-    if (elf == NULL) {
-        fprintf(stderr, "elf_begin() failed\n");
-        exit(1);
-    }
+/**
+ * @brief Prints ELF symbol table.
+ * @param elf ELF file
+ */
+void print_symbols(Elf *elf) {
+    GElf_Ehdr ehdr;
+    GElf_Shdr shdr;
+    Elf_Scn *scn = NULL;
+    Elf_Data *data;
+    Elf32_Sym *sym32;
+    Elf64_Sym *sym64;
 
     // Get the ELF header
-    GElf_Ehdr ehdr;
     if (gelf_getehdr(elf, &ehdr) == NULL) {
-        fprintf(stderr, "gelf_getehdr() failed\n");
-        exit(1);
+        errx(elf_errno(), "gelf_getehdr() failed: %s\n", elf_errmsg(-1));
     }
 
-    // Print the ELF header
-    printf("ELF Header:\n");
-    printf("  Magic: ");
-    for (int i = 0; i < EI_NIDENT; i++) {
-        printf("%02x ", ehdr.e_ident[i]);
-    }
-    printf("\n");
-    printf("  Class: %d-bit\n", ehdr.e_ident[EI_CLASS] == ELFCLASS32 ? 32 : 64);
-    printf("  Data: %s\n", ehdr.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-    printf("  Version: %d\n", ehdr.e_ident[EI_VERSION]);
-    printf("  OS/ABI: %d\n", ehdr.e_ident[EI_OSABI]);
-    printf("  ABI Version: %d\n", ehdr.e_ident[EI_ABIVERSION]);
-    printf("  Type: %d\n", ehdr.e_type);
-    printf("  Machine: %d\n", ehdr.e_machine);
-    printf("  Version: %d\n", ehdr.e_version);
-    printf("  Entry point address: 0x%lx\n", ehdr.e_entry);
-    printf("  Start of program headers: %ld (bytes into file)\n", ehdr.e_phoff);
-    printf("  Start of section headers: %ld (bytes into file)\n", ehdr.e_shoff);
-    printf("  Flags: 0x%x\n", ehdr.e_flags);
-    printf("  Size of this header: %d (bytes)\n", ehdr.e_ehsize);
-    printf("  Size of program headers: %d (bytes)\n", ehdr.e_phentsize);
-    printf("  Number of program headers: %d\n", ehdr.e_phnum);
-    printf("  Size of section headers: %d (bytes)\n", ehdr.e_shentsize);
-    printf("  Number of section headers: %d\n", ehdr.e_shnum);
-    printf("  Section header string table index: %d\n", ehdr.e_shstrndx);
+    // Search all sections
+    while ((scn = elf_nextscn(elf, scn)) != NULL) {
+        if (gelf_getshdr(scn, &shdr) != &shdr)
+            continue;
 
-    // Close the ELF file
+        // Check if section contains symbol table
+        if (shdr.sh_type == SHT_SYMTAB || shdr.sh_type == SHT_DYNSYM) {
+
+            data = elf_getdata(scn, NULL);
+            int symbols = shdr.sh_size / shdr.sh_entsize;
+
+            if (data) {
+
+                // Determine if symbols are 32 or 64 bit
+                if (shdr.sh_entsize == sizeof(Elf32_Sym)) {
+                    sym32 = (Elf32_Sym *) data->d_buf;
+                    sym64 = NULL;
+                } else {
+                    sym32 = NULL;
+                    sym64 = (Elf64_Sym *) data->d_buf;
+                }
+
+                // Print the symbol table
+                printf("%-20s %s\n", "Name", "Value");
+                for (int i = 0; i < symbols; ++i) {
+                    if (sym32) {
+                        if (sym32[i].st_shndx == SHN_UNDEF || sym32[i].st_shndx == SHN_COMMON ||
+                            sym32[i].st_shndx == SHN_ABS) {
+                            continue;
+                        }
+                        Elf_Scn *data_scn = elf_getscn(elf, sym32[i].st_shndx);
+                        if (data_scn != NULL) {
+                            Elf_Data *data_data = elf_getdata(data_scn, NULL);
+                            if (data_data != NULL) {
+                                if (GELF_ST_TYPE(sym32[i].st_info) == STT_OBJECT) {
+                                    printf("%-20s %d\n", elf_strptr(elf, shdr.sh_link, sym32[i].st_name),
+                                           (((int*)data_data->d_buf)[sym32[i].st_value/sizeof (int)]));
+                                }
+                            } else {
+                                printf("%-20s %s\n", elf_strptr(elf, shdr.sh_link, sym32[i].st_name), "NULL");
+                            }
+                        }
+                    } else if (sym64) {
+                        if (sym64[i].st_shndx == SHN_UNDEF || sym64[i].st_shndx == SHN_COMMON ||
+                            sym64[i].st_shndx == SHN_ABS) {
+                            continue;
+                        }
+                        Elf_Scn *data_scn = elf_getscn(elf, sym64[i].st_shndx);
+                        if (data_scn != NULL) {
+                            Elf_Data *data_data = elf_getdata(data_scn, NULL);
+                            if (data_data != NULL) {
+                                if (GELF_ST_TYPE(sym64[i].st_info) == STT_OBJECT) {
+                                    printf("%-20s %d\n", elf_strptr(elf, shdr.sh_link, sym64[i].st_name),
+                                           (int)(((int*)data_data->d_buf)[sym64[i].st_value/sizeof (int)]));
+                                }
+                            } else {
+                                printf("%-20s %s\n", elf_strptr(elf, shdr.sh_link, sym64[i].st_name), "NULL");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        errx(1, "Usage: ian_proj1 FILE");
+    }
+
+    int fd = open(argv[1], O_RDONLY, 0);
+    if (fd < 0) {
+        errx(1, "open() failed");
+    }
+
+    if (elf_version(EV_CURRENT) == EV_NONE) {
+        errx(elf_errno(), "ELF library initialization failed: %s", elf_errmsg(-1));
+    }
+
+    Elf *elf = elf_begin(fd, ELF_C_READ, NULL);
+    if (elf == NULL) {
+        errx(elf_errno(), "elf_begin() failed: %s", elf_errmsg(-1));
+    }
+
+    print_symbols(elf);
+
     elf_end(elf);
     close(fd);
 
